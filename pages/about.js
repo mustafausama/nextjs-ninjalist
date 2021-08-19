41a33d3fd44d1152518baf5be63590ce85b5b4de
Mustafa usama <mustafausama@outlook.com>
2021-08-19 04:13:04 +0200

    Initial

---
+++ pages/about.js
@@ -0,0 +1,35 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Sunt excepteur sit ullamco cillum consequat quis. Veniam aliquip duis
+          proident culpa sint consequat veniam culpa exercitation excepteur eu
+          ut ut ea. Proident in sint sit duis est velit consequat aliqua tempor.
+          Duis qui non incididunt laborum eiusmod fugiat. Commodo occaecat est
+          culpa occaecat consectetur culpa eu consectetur consectetur
+          consectetur. Incididunt exercitation non duis occaecat culpa tempor
+          occaecat magna esse. Nisi officia do anim ad excepteur veniam dolore.
+        </p>
+        <p>
+          Sunt excepteur sit ullamco cillum consequat quis. Veniam aliquip duis
+          proident culpa sint consequat veniam culpa exercitation excepteur eu
+          ut ut ea. Proident in sint sit duis est velit consequat aliqua tempor.
+          Duis qui non incididunt laborum eiusmod fugiat. Commodo occaecat est
+          culpa occaecat consectetur culpa eu consectetur consectetur
+          consectetur. Incididunt exercitation non duis occaecat culpa tempor
+          occaecat magna esse. Nisi officia do anim ad excepteur veniam dolore.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
